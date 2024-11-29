4df5e9bef623be50e694308f5559aee0a2bc060f
gregory <grigorizagladkin@gmail.com>
2024-11-29 18:11:05 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+const Page = async () => {
+  return (
+    <div className="w-full h-full overflow-hidden">
+      <div className="h-screen mx-auto max-w-[800px] shadow-sm pt-20 px-3">
+        <h1 className="text-2xl font-light">
+          Как заработать с рефералки тинькофф?
+        </h1>
+        <p className="mt-5">
+          У тинькофф существует реферальная программа(приведи друга) по которой
+          я, как владелец реферальной ссылки могу получить вознаграждение за
+          заказ дебетовой или кредитной карты(или иного продукта тинькоффа,
+          полный список будет ниже). Для того чтобы
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
