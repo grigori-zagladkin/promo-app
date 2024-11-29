413996235041622d1a0dd448980601b8e1ed3a15
gregory <grigorizagladkin@gmail.com>
2024-11-29 18:57:24 +0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,53 @@ const Page = async () => {
     <div className="w-full h-full overflow-hidden">
       <div className="h-screen mx-auto max-w-[800px] shadow-sm pt-20 px-3">
         <h1 className="text-2xl font-light">
-          Как заработать с рефералки тинькофф?
+          Как заработать с рефералки т-банка?
         </h1>
         <p className="mt-5">
           У тинькофф существует реферальная программа(приведи друга) по которой
           я, как владелец реферальной ссылки могу получить вознаграждение за
           заказ дебетовой или кредитной карты(или иного продукта тинькоффа,
-          полный список будет ниже). Для того чтобы
+          полный список будет в рефералке). Для того чтобы заполучить желанные
+          денежки, нужно заказать по{" "}
+          <a
+            className="text-neutral-400 underline"
+            href="https://www.tbank.ru/baf/6ok4GD5vnp9"
+          >
+            ссылке
+          </a>{" "}
+          карту, и выполнить условие, как правило потратить 300-500 рублей,
+          продуктов купить, например :) И через время, неделя-две в среднем, мне
+          приходит вознаграждение и мы делим его пополам.Для связи со мной можно
+          использовать{" "}
+          <a
+            className="text-neutral-400 underline"
+            href="https://t.me/Saint_Gregory"
+          >
+            телеграмм
+          </a>{" "}
+          или{" "}
+          <a
+            className="text-neutral-400 underline"
+            href="https://vk.com/id491549316"
+          >
+            вк
+          </a>
+          . Пишите +- через неделю после выполнения условия, если денежка за вас
+          пришла я вам переведу.
         </p>
+        <h2 className="text-2xl font-light mt-5">Внимание, акция!</h2>
+        <p className="mt-5">
+          Если заказать{" "}
+          <a
+            className="text-neutral-400 underline"
+            href="https://www.tbank.ru/cards/debit-cards/tinkoff-black/mgm/?utm_source=mgm_mb_android&_deep_link_sub1=2Ub7jg1mFR5&short_link=2Ub7jg1mFR5&httpMethod=GET"
+          >
+            дебетовую карту
+          </a>
+          , то вы получите 500р вознаграждения + я 2000(делим пополам = 1000),
+          соответственно суммарно вы получите 1500р.
+        </p>
+        <p className="mt-10 font-light">Актуально на 29.11.24</p>
       </div>
     </div>
   );
